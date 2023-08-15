9387935acd351e712de83ae06a50b9f3746ca037
Valeriy <masterbox06@gmail.com>
2023-08-15 19:15:57 +0400

    Added input error style logic, changing isDone logic, input trim logic

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,9 +5,11 @@ import {Button} from "./Button";
 type PropsType = {
   header: string
   tasks: Array<Task>
-  removeTaskFunc: (id: string) => void
-  changeFilterFunc: (value: FilterValuesType) => void
+  removeTask: (id: string) => void
+  changeFilter: (value: FilterValuesType) => void
   addTask: (title: string) => void
+  changeIsDone: (id: string, isDone: boolean) => void
+  activeFilter: FilterValuesType
 }
 type Task = {
   id: string
@@ -17,15 +19,22 @@ type Task = {
 
 export const TodoList = (props: PropsType) => {
   const [inputValue, setInputValue] = useState("")
+  const [error, setError] = useState<string | null>(null)
 
-  const changeFilterToStateHandler = (state: FilterValuesType) => props.changeFilterFunc(state)
-  const removeTaskHandler = (id: string) => props.removeTaskFunc(id)
-  const inputKeyPressHandler = (e: React.KeyboardEvent<HTMLInputElement>) => e.key === "Enter" && addTaskHandler()
-  const inputOnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.currentTarget.value)
-  const addTaskHandler = () => {
-    props.addTask(inputValue)
+  const changeFilterToStateHandler = (state: FilterValuesType) => {
+    props.changeFilter(state)
+  }
+  const inputKeyPressHandler = (e: React.KeyboardEvent<HTMLInputElement>) => e.key === "Enter" && addTask()
+  const inputOnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setError(null)
+    setInputValue(e.currentTarget.value)
+  }
+  const addTask = () => {
+    if(!inputValue.trim()) setError("This field is required")
+    props.addTask(inputValue.trim())
     setInputValue("")
   }
+  const changeIsDoneHandler = (id: string, checked: boolean) => props.changeIsDone(id, checked)
 
   return (
     <div>
@@ -35,25 +44,30 @@ export const TodoList = (props: PropsType) => {
           value={inputValue}
           onChange={(e) => inputOnChangeHandler(e)}
           onKeyDown={(e) => inputKeyPressHandler(e)}
+          className={error ? "error" : ""}
         />
-        <Button callBack={addTaskHandler} title={"+"}/>
+        <Button callBack={addTask} title={"+"}/>
       </div>
+      {error && <div className={"error-message"}>{error}</div>}
       <div style={{display: "flex", justifyContent: "space-between", marginTop: "10px"}}>
-        <Button callBack={() => changeFilterToStateHandler("all")} title={"All"}/>
-        <Button callBack={() => changeFilterToStateHandler("active")} title={"Active"}/>
-        <Button callBack={() => changeFilterToStateHandler("completed")} title={"Completed"}/>
+        <Button active={props.activeFilter === "all"} callBack={() => changeFilterToStateHandler("all")} title={"All"}/>
+        <Button active={props.activeFilter === "active"} callBack={() => changeFilterToStateHandler("active")} title={"Active"}/>
+        <Button active={props.activeFilter === "completed"} callBack={() => changeFilterToStateHandler("completed")} title={"Completed"}/>
       </div>
       <ul style={{display: "flex", flexDirection: "column", margin: "20px 0", padding: 0, gap: "5px"}}>
         {
           props.tasks.map(task => {
+            const removeTaskHandler = (id: string) => props.removeTask(id)
             return (
               <li
                 key={task.id}
                 style={{display: "flex", justifyContent: "space-between"}}
+                className={task.isDone ? "is-done" : ""}
               >
                 <input
                   type="checkbox"
-                  defaultChecked={task.isDone}
+                  checked={task.isDone}
+                  onChange={(e)=>changeIsDoneHandler(task.id, e.currentTarget.checked)}
                 />
                 <span>{task.title}</span>
                 <Button callBack={() => removeTaskHandler(task.id)} title={"x"}/>
