6ee0f502b3262b9d61aa4bfa6fb52e5faa53036a
Valeriy <masterbox06@gmail.com>
2023-09-12 16:40:24 +0300

    renamed from reducer.test.ts

--- src/state/todoList-reducer.test.ts
+++ src/state/todoList-reducer.test.ts
@@ -1,8 +1,8 @@
 import {
-  AddTodoListAC,
-  ChangeTodoListFilterAC,
-  RemoveTodoListAC,
-  RenameTodoListAC,
+  addTodoListAC,
+  changeTodoListFilterAC,
+  removeTodoListAC,
+  renameTodoListAC,
   todoListReducer
 } from './todoList-reducer'
 import {TodolistType} from "../App";
@@ -13,28 +13,28 @@ const startState: TodolistType[] = [
 ]
 
 test('should remove todo', () => {
-  const endState = todoListReducer(startState, RemoveTodoListAC("2"))
+  const endState = todoListReducer(startState, removeTodoListAC("2"))
 
   expect(endState.length).toBe(1)
   expect(endState[0].id).toBe("1")
 })
 
 test('should add todo', () => {
-  const endState = todoListReducer(startState, AddTodoListAC("new todo"))
+  const endState = todoListReducer(startState, addTodoListAC("new todo"))
 
   expect(endState.length).toBe(3)
   expect(endState[0].title).toBe("new todo")
 })
 
 test('should rename todo', () => {
-  const endState = todoListReducer(startState, RenameTodoListAC("2", "new title"))
+  const endState = todoListReducer(startState, renameTodoListAC("2", "new title"))
 
   expect(endState.length).toBe(2)
   expect(endState[1].title).toBe("new title")
 })
 
 test('should change todo filter', () => {
-  const endState = todoListReducer(startState, ChangeTodoListFilterAC("2", "active"))
+  const endState = todoListReducer(startState, changeTodoListFilterAC("2", "active"))
 
   expect(endState.length).toBe(2)
   expect(endState[1].filter).toBe("active")
