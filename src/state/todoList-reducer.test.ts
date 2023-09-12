ba6f1b0627fbed26927ea748b5773ad7b5246f81
Valeriy <masterbox06@gmail.com>
2023-09-12 18:01:21 +0300

    added updating tasks state in test "should add todo"

--- src/state/todoList-reducer.test.ts
+++ src/state/todoList-reducer.test.ts
@@ -5,12 +5,20 @@ import {
   renameTodoListAC,
   todoListReducer
 } from './todoList-reducer'
-import {TodolistType} from "../App";
+import {TasksStateType, TodolistType} from "../App";
+import {v1} from "uuid";
+import {addEmptyTodoListAC, taskReducer} from "./task-reducer";
 
 const startState: TodolistType[] = [
   {id: "1", title: "What to learn", filter: "all"},
   {id: "2", title: "What to buy", filter: "all"},
 ]
+const startTasks: TasksStateType = {
+  ["1"]: [
+    {id: v1(), title: "HTML&CSS", isDone: false},
+    {id: v1(), title: "JS", isDone: true},
+  ]
+}
 
 test('should remove todo', () => {
   const endState = todoListReducer(startState, removeTodoListAC("2"))
@@ -21,10 +29,12 @@ test('should remove todo', () => {
 
 test('should add todo', () => {
   const endState = todoListReducer(startState, addTodoListAC("3", "new todo"))
+  const endTasks = taskReducer(startTasks, addEmptyTodoListAC("3"))
 
   expect(endState.length).toBe(3)
   expect(endState[0].title).toBe("new todo")
   expect(endState[0].id).toBe("3")
+  expect(endTasks["3"].length).toBe(0)
 })
 
 test('should rename todo', () => {
