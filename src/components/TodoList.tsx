6f2f724f4c4005fe7c0d099c1efa05c5ea5f7a33
Valeriy <masterbox06@gmail.com>
2023-09-26 17:40:46 +0300

    added tasks redux logic

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,93 +1,84 @@
 import React from "react";
-import {FilterValuesType, Task} from "../App";
+import {FilterValuesType} from "../App";
 import {AddItemForm} from "./AddItemForm";
 import {EditableSpan} from "./EditableSpan";
 import {Button, Checkbox, IconButton, List, ListItem} from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../state/store";
+import {addTaskAC, changeTaskIsDoneAC, removeTaskAC, renameTaskAC} from "../state/task-reducer";
+import {changeTodoListFilterAC, removeTodoListAC, renameTodoListAC} from "../state/todoList-reducer";
 
 type PropsType = {
   todoListId: string
   title: string
-  tasks: Task[]
-  activeFilter: FilterValuesType
-
-  // CRUD tasks
-  addTask: (todoListId: string, title: string) => void
-  renameTask: (todoListId: string, id: string, title: string) => void
-  changeIsDone: (todoListId: string, id: string, isDone: boolean) => void
-  removeTask: (todoListId: string, id: string) => void
-
-  // CRUD TodoLists
-  addTodoList: (title: string) => void
-  renameTodoList: (todoListId: string, title: string) => void
-  changeFilter: (todoListId: string, filter: FilterValuesType) => void
-  removeTodoList: (todoListId: string) => void
+  filter: FilterValuesType
+}
+export type Task = {
+  id: string
+  title: string
+  isDone: boolean
+}
+export type TasksType = {
+  [todoListId: string]: Task[]
 }
 
-export const TodoList = (props: PropsType) => {
-  // Handlers
-  const addTaskHandler = (title: string) => {
-    props.addTask(props.todoListId, title)
-  }
-  const changeFilterToStateHandler = (state: FilterValuesType) => {
-    props.changeFilter(props.todoListId, state)
-  }
-  const changeTodoListTitle = (title: string) => {
-    props.renameTodoList(props.todoListId, title)
-  }
-  const removeTodolistHandler = () => {
-    props.removeTodoList(props.todoListId)
+
+export const TodoList = ({todoListId, title, filter}: PropsType) => {
+  const dispatch = useDispatch()
+  const tasks = useSelector<RootState, Task[]>(state => state.tasks[todoListId])
+
+  const getFilteredTasks = (tasks: Task[], filter: FilterValuesType) => {
+    switch (filter) {
+      case "active":
+        return tasks.filter(task => !task.isDone)
+      case "completed":
+        return tasks.filter(task => task.isDone)
+      default:
+        return tasks
+    }
   }
 
-  const tasksList = props.tasks ? props.tasks.map(task => {
-    const removeTaskHandler = (id: string) => props.removeTask(props.todoListId, id)
-    const changeIsDoneHandler = (id: string, checked: boolean) => {
-      props.changeIsDone(props.todoListId, id, checked)
-    }
-    const changeTaskTitle = (title: string) => {
-      props.renameTask(props.todoListId, task.id, title)
-    }
-
-    return (
-      <ListItem key={task.id} className={task.isDone ? "is-done" : ""} style={{display: "flex", justifyContent: "space-between"}}>
-        <Checkbox
-          checked={task.isDone}
-          onChange={(e) => changeIsDoneHandler(task.id, e.currentTarget.checked)}
-          icon={<CheckBoxOutlineBlankIcon/>}
-          checkedIcon={<CheckBoxIcon/>}/>
-        <EditableSpan title={task.title} changeTitle={changeTaskTitle}/>
-        <IconButton aria-label="delete" onClick={() => removeTaskHandler(task.id)}>
-          <DeleteIcon/>
-        </IconButton>
-      </ListItem>
-    )
-  }) : null
+  const filteredTasks = getFilteredTasks(tasks, filter)
 
   return (
     <div style={{display: "flex", flexDirection: "column", gap: "1rem"}}>
       <h3 style={{display: "flex", justifyContent: "space-between"}}>
-        <EditableSpan title={props.title} changeTitle={changeTodoListTitle}/>
-        <IconButton aria-label="delete" onClick={removeTodolistHandler}>
+        <EditableSpan title={title} changeTitle={(title: string) => dispatch(renameTodoListAC(todoListId, title))}/>
+        <IconButton aria-label="delete" onClick={() => dispatch(removeTodoListAC(todoListId))}>
           <DeleteIcon/>
         </IconButton>
       </h3>
-      <AddItemForm addItem={addTaskHandler} maxInputLength={15}/>
+      <AddItemForm addItem={(title: string) => dispatch(addTaskAC(todoListId, title))} maxInputLength={15}/>
       <div style={{display: "flex", justifyContent: "space-between"}}>
-        <Button variant={props.activeFilter === "all" ? "contained" : "outlined"}
-                onClick={() => changeFilterToStateHandler("all")}>All</Button>
-        <Button variant={props.activeFilter === "active" ? "contained" : "outlined"}
-                onClick={() => changeFilterToStateHandler("active")}>Active</Button>
-        <Button variant={props.activeFilter === "completed" ? "contained" : "outlined"}
-                onClick={() => changeFilterToStateHandler("completed")}>Completed</Button>
+        <Button variant={filter === "all" ? "contained" : "outlined"}
+                onClick={() => dispatch(changeTodoListFilterAC(todoListId, "all"))}>All</Button>
+        <Button variant={filter === "active" ? "contained" : "outlined"}
+                onClick={() => dispatch(changeTodoListFilterAC(todoListId, "active"))}>Active</Button>
+        <Button variant={filter === "completed" ? "contained" : "outlined"}
+                onClick={() => dispatch(changeTodoListFilterAC(todoListId, "completed"))}>Completed</Button>
       </div>
       <List>
-        {tasksList}
+        {
+          filteredTasks.map(task =>
+            <ListItem key={task.id} className={task.isDone ? "is-done" : ""}
+                      style={{display: "flex", justifyContent: "space-between"}}>
+              <Checkbox
+                checked={task.isDone}
+                onChange={(e) => dispatch(changeTaskIsDoneAC(todoListId, task.id, e.currentTarget.checked))}
+                icon={<CheckBoxOutlineBlankIcon/>}
+                checkedIcon={<CheckBoxIcon/>}/>
+              <EditableSpan title={task.title}
+                            changeTitle={(title: string) => dispatch(renameTaskAC(todoListId, task.id, title))}/>
+              <IconButton aria-label="delete" onClick={() => dispatch(removeTaskAC(todoListId, task.id))}>
+                <DeleteIcon/>
+              </IconButton>
+            </ListItem>
+          )
+        }
       </List>
-      <ul className="todolist-ul">
-
-      </ul>
     </div>
   )
 }
