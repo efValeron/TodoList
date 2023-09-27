4a77cbf1b816793c9e392614b8ee623f12f0d9a5
Valeriy <masterbox06@gmail.com>
2023-09-27 21:23:51 +0300

    memoized, added useCallback and useMemo, Button changed to CustomButton, added Task component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,36 +1,37 @@
-import React from "react";
+import React, {memo, useCallback, useMemo} from "react";
 import {FilterValuesType} from "../App";
 import {AddItemForm} from "./AddItemForm";
 import {EditableSpan} from "./EditableSpan";
-import {Button, Checkbox, IconButton, List, ListItem} from "@mui/material";
+import {IconButton, List} from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../state/store";
-import {addTaskAC, changeTaskIsDoneAC, removeTaskAC, renameTaskAC} from "../state/task-reducer";
+import {addTaskAC} from "../state/task-reducer";
 import {changeTodoListFilterAC, removeTodoListAC, renameTodoListAC} from "../state/todoList-reducer";
+import {CustomButton} from "./CustomButton";
+import {Task} from "./Task";
 
 type PropsType = {
   todoListId: string
   title: string
   filter: FilterValuesType
 }
-export type Task = {
+export type TaskType = {
   id: string
   title: string
   isDone: boolean
 }
 export type TasksType = {
-  [todoListId: string]: Task[]
+  [todoListId: string]: TaskType[]
 }
 
 
-export const TodoList = ({todoListId, title, filter}: PropsType) => {
+export const TodoList = memo(({todoListId, title, filter}: PropsType) => {
   const dispatch = useDispatch()
-  const tasks = useSelector<RootState, Task[]>(state => state.tasks[todoListId])
+  const tasks = useSelector<RootState, TaskType[]>(state => state.tasks[todoListId])
 
-  const getFilteredTasks = (tasks: Task[], filter: FilterValuesType) => {
+  const addTask = useCallback((title: string) => dispatch(addTaskAC(todoListId, title)), [dispatch, todoListId])
+  const getFilteredTasks = (tasks: TaskType[], filter: FilterValuesType) => {
     switch (filter) {
       case "active":
         return tasks.filter(task => !task.isDone)
@@ -41,44 +42,34 @@ export const TodoList = ({todoListId, title, filter}: PropsType) => {
     }
   }
 
-  const filteredTasks = getFilteredTasks(tasks, filter)
+  const filteredTasks = useMemo(() => getFilteredTasks(tasks, filter), [tasks, filter])
 
   return (
     <div style={{display: "flex", flexDirection: "column", gap: "1rem"}}>
       <h3 style={{display: "flex", justifyContent: "space-between"}}>
-        <EditableSpan title={title} changeTitle={(title: string) => dispatch(renameTodoListAC(todoListId, title))}/>
+        <EditableSpan title={title}
+                      changeTitle={useCallback((title: string) => dispatch(renameTodoListAC(todoListId, title)), [dispatch, todoListId])}/>
         <IconButton aria-label="delete" onClick={() => dispatch(removeTodoListAC(todoListId))}>
           <DeleteIcon/>
         </IconButton>
       </h3>
-      <AddItemForm addItem={(title: string) => dispatch(addTaskAC(todoListId, title))} maxInputLength={15}/>
+      <AddItemForm addItem={addTask} maxInputLength={15}/>
       <div style={{display: "flex", justifyContent: "space-between"}}>
-        <Button variant={filter === "all" ? "contained" : "outlined"}
-                onClick={() => dispatch(changeTodoListFilterAC(todoListId, "all"))}>All</Button>
-        <Button variant={filter === "active" ? "contained" : "outlined"}
-                onClick={() => dispatch(changeTodoListFilterAC(todoListId, "active"))}>Active</Button>
-        <Button variant={filter === "completed" ? "contained" : "outlined"}
-                onClick={() => dispatch(changeTodoListFilterAC(todoListId, "completed"))}>Completed</Button>
+        <CustomButton variant={filter === "all" ? "contained" : "outlined"}
+                      onClick={useCallback(() => dispatch(changeTodoListFilterAC(todoListId, "all")), [dispatch, todoListId])}
+        >All</CustomButton>
+        <CustomButton variant={filter === "active" ? "contained" : "outlined"}
+                      onClick={useCallback(() => dispatch(changeTodoListFilterAC(todoListId, "active")), [dispatch, todoListId])}
+        >Active</CustomButton>
+        <CustomButton variant={filter === "completed" ? "contained" : "outlined"}
+                      onClick={useCallback(() => dispatch(changeTodoListFilterAC(todoListId, "completed")), [dispatch, todoListId])}
+        >Completed</CustomButton>
       </div>
       <List>
         {
-          filteredTasks.map(task =>
-            <ListItem key={task.id} className={task.isDone ? "is-done" : ""}
-                      style={{display: "flex", justifyContent: "space-between"}}>
-              <Checkbox
-                checked={task.isDone}
-                onChange={(e) => dispatch(changeTaskIsDoneAC(todoListId, task.id, e.currentTarget.checked))}
-                icon={<CheckBoxOutlineBlankIcon/>}
-                checkedIcon={<CheckBoxIcon/>}/>
-              <EditableSpan title={task.title}
-                            changeTitle={(title: string) => dispatch(renameTaskAC(todoListId, task.id, title))}/>
-              <IconButton aria-label="delete" onClick={() => dispatch(removeTaskAC(todoListId, task.id))}>
-                <DeleteIcon/>
-              </IconButton>
-            </ListItem>
-          )
+          filteredTasks.map(task => <Task key={task.id} task={task} todoListId={todoListId}/>)
         }
       </List>
     </div>
   )
-}
+})
